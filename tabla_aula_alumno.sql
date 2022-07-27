63214b042371c57882a0c0166c50818bdaad84ec
Jaime Ttito <jaime.tticco.08@gmail.com>
2022-07-26 22:22:27 -0500

    Actulizardo

--- tabla_aula_alumno.sql
+++ tabla_aula_alumno.sql
@@ -8,13 +8,13 @@ USE `coledio_idat` ;
 
 DROP TABLE IF EXISTS aula CASCADE;
 CREATE TABLE aula(
-	id_aula Integer PRIMARY KEY,
+	id_aula INT auto_increment PRIMARY KEY ,
 	nombre VARCHAR(180) NOT NULL,	
 );
 
 DROP TABLE IF EXISTS alumno CASCADE;
 CREATE TABLE alumno(
-	id_alumno Integer PRIMARY KEY,
+	id_alumno INT auto_increment PRIMARY KEY,
 	apellido VARCHAR(255) NOT NULL,
 	nombre VARCHAR(255) NOT NULL,
 	
@@ -22,8 +22,8 @@ CREATE TABLE alumno(
 
 DROP TABLE IF EXISTS detalle_aula CASCADE;
 CREATE TABLE detalle_aula(
-	id_aula Integer NOT NULL,
-	id_alumno Integer NOT NULL,	
+	id_aula INT NOT NULL,
+	id_alumno INT NOT NULL,	
 	FOREIGN KEY(id_aula) REFERENCES aula(id_aula) ON UPDATE CASCADE ON DELETE CASCADE,
 	FOREIGN KEY(id_alumno) REFERENCES alumno(id_alumno) ON UPDATE CASCADE ON DELETE CASCADE,
 	PRIMARY KEY(id_aula, id_alumno)
